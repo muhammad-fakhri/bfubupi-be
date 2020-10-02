98b28e2b28576c413e2f26e48cae1061862f3335
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-10-02 18:09:52 +0700

    Set up for jwt token

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable, HasFactory;
 
@@ -20,6 +21,11 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      */
     protected $fillable = ['name', 'school_name', 'email', 'password'];
 
+    /**
+     * Relationship functions
+     *
+     * @return array
+     */
     public function payments()
     {
         return $this->hasMany('App\Payment');
@@ -37,5 +43,30 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      */
     protected $hidden = [
         'password',
+        'email_verify_token',
+        'email_verify_date',
+        'change_password_token',
+        'created_at',
+        'updated_at',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
