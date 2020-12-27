27a9ccc172b82cc3b1e34d17219210a8e126b9ba
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2020-12-27 16:31:12 +0700

    Add created_at and updated_at value in LinkSeeder

--- database/seeders/LinkSeeder.php
+++ database/seeders/LinkSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,21 +16,96 @@ class LinkSeeder extends Seeder
     public function run()
     {
         DB::table('links')->insert([
-            ['code' => 'regis_lktin', 'value' => 'https://drive.google.com'],
-            ['code' => 'regis_micro', 'value' => 'https://drive.google.com'],
-            ['code' => 'regis_olim_sains', 'value' => 'https://drive.google.com'],
-            ['code' => 'regis_olim_bio', 'value' => 'https://drive.google.com'],
-            ['code' => 'regis_lct_bio', 'value' => 'https://drive.google.com'],
-            ['code' => 'upload_payment', 'value' => 'https://drive.google.com'],
-            ['code' => 'upload_paper', 'value' => 'https://drive.google.com'],
-            ['code' => 'rulebook_lktin', 'value' => 'https://drive.google.com'],
-            ['code' => 'rulebook_micro', 'value' => 'https://drive.google.com'],
-            ['code' => 'rulebook_olim_sains', 'value' => 'https://drive.google.com'],
-            ['code' => 'rulebook_olim_bio', 'value' => 'https://drive.google.com'],
-            ['code' => 'rulebook_lct_bio', 'value' => 'https://drive.google.com'],
-            ['code' => 'timeline', 'value' => 'https://drive.google.com'],
-            ['code' => 'bank_soal', 'value' => 'https://drive.google.com'],
-            ['code' => 'template', 'value' => 'https://drive.google.com'],
+            [
+                'code' => 'regis_lktin',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'regis_micro',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'regis_olim_sains',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'regis_olim_bio',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'regis_lct_bio',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'upload_payment',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'upload_paper',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'rulebook_lktin',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'rulebook_micro',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'rulebook_olim_sains',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'rulebook_olim_bio',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'rulebook_lct_bio',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'timeline',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'bank_soal',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'code' => 'template',
+                'value' => 'https://drive.google.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
         ]);
     }
 }
