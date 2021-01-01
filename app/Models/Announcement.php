19fd8a3172c06e07b4d2652ee262f3fed1cc01f0
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2021-01-01 18:57:14 +0700

    Update announcement model

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    protected $fillable = ['title', 'content', 'show'];
+    protected $fillable = ['link', 'content', 'show'];
     protected $hidden = ['created_at', 'updated_at'];
 }
