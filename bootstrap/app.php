66fa4d8b661ca12ab7f2bf20c1f54b67474bd7b2
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2021-01-05 23:06:44 +0700

    Revert back cors disable

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,11 +72,9 @@ $app->configure('app');
 |
 */
 
-if (env('APP_ENV') == "local") {
-    $app->middleware([
-        Fruitcake\Cors\HandleCors::class,
-    ]);
-}
+$app->middleware([
+    Fruitcake\Cors\HandleCors::class,
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
@@ -103,10 +101,8 @@ $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Illuminate\Mail\MailServiceProvider::class);
-if (env('APP_ENV') == "local") {
-    $app->register(Fruitcake\Cors\CorsServiceProvider::class);
-    $app->configure('cors');
-}
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
+$app->configure('cors');
 
 $app->configure('mail');
 $app->alias('mailer', Illuminate\Mail\Mailer::class);
